f8dce1fba2efa5db610152ea9a48235f2acd89fa
ccjavad <ccjavad@gmail.com>
2024-11-13 11:17:42 +0100

    update

--- Projekte/Threads/src/aufgaben/AufgabeThreadsSimpleAPI.java
+++ Projekte/Threads/src/aufgaben/AufgabeThreadsSimpleAPI.java
@@ -1,17 +1,75 @@
 package aufgaben;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import threads.MyThreadUtils;
 
 public class AufgabeThreadsSimpleAPI {
 
 	public static void main(String[] args) {
 //		a1();
-		a2();
+//		a2();
+		a3();
+//		a4();
+		a4alternativ();
+	}
+	
+	static void a4alternativ() {
+		
+		for (char ch = 'A'; ch<='Z'; ch++) {
+			char chCopy = ch; 
+			Runnable task = () -> System.out.println(chCopy); 
+			Thread th = new Thread(task);
+			th.start();
+		}
+	}
+	
+	static void a4() {
+		class CharPrinter implements Runnable {
+			char ch;
+			
+			public CharPrinter(char ch) {
+				this.ch = ch;
+			}
+
+			public void run() {
+				System.out.println(ch);
+			}
+		}
+		
+		for (char ch = 'A'; ch<='Z'; ch++) {
+			Runnable task = new CharPrinter(ch); 
+			Thread th = new Thread(task);
+			th.start();
+		}
+	}
+	
+	static void a3() {
+		Runnable task = () -> {
+			Thread curThread = Thread.currentThread();
+			System.out.println(curThread.getName() + " " + curThread.getId());
+		};
+		
+		List<Thread> threads = new ArrayList<>();
+		
+		for (int i = 0; i < 37; i++) {
+		 	threads.add( new Thread(task) );
+		}
+		
+		for (Thread th : threads) {
+			th.start();
+		}
 	}
 
 	static void a2() {
 		Runnable task = () -> {
+			Thread curThread = Thread.currentThread();
 			
+			while(true) {
+				System.out.println(curThread.getName() + " " + curThread.getId() + " läuft...");
+				MyThreadUtils.pause(1000);
+			}
 		};
 		
 		Thread th = new Thread(task);
