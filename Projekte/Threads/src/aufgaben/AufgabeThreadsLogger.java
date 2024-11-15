da4ed253baff6db8f9f4e44910807ed8ae876257
ccjavad <ccjavad@gmail.com>
2024-11-15 11:16:38 +0100

    update

--- Projekte/Threads/src/aufgaben/AufgabeThreadsLogger.java
+++ Projekte/Threads/src/aufgaben/AufgabeThreadsLogger.java
@@ -1,10 +1,45 @@
 package aufgaben;
 
+class MyLogger {
+	private StringBuilder sb = new StringBuilder();
+//	private StringBuffer sb = new StringBuffer();
+	
+	public synchronized void addMessage(String caller, String message) {
+		sb.append(caller)
+			.append(" - ")
+			.append(message)
+			.append("\n");
+	}
+	
+	public synchronized String getLog() {
+		return sb.toString();
+	}
+}
+
 public class AufgabeThreadsLogger {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
 
+		MyLogger logger = new MyLogger();
+		
+		Runnable task = () -> {
+			String caller = Thread.currentThread().getName();
+			String message = "Msg ";
+			for (int i = 0; i < 10; i++) {
+				logger.addMessage(caller, message+i);
+			}
+		};
+		
+		Thread tA = new Thread(task);
+		Thread tB = new Thread(task);
+
+		tA.start();
+		tB.start();
+		
+		tA.join();
+		tB.join();
+		
+		System.out.println(logger.getLog());
 	}
 
 }
