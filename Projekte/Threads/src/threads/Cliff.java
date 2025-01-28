76d8c17e21533b754c1c2b7d05930e171638cad1
mahdinezhadasad <mahdinezhadasad@gmail.com>
2025-01-28 09:05:57 +0100

    ocp files

--- Projekte/Threads/src/threads/Cliff.java
+++ Projekte/Threads/src/threads/Cliff.java
@@ -1,5 +1,15 @@
 package Threads.src.threads;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
+
 final class FallenException extends Exception{}
 
 final class HikinGear implements AutoCloseable {
@@ -23,7 +33,7 @@ public class Cliff {
     }
     
     public static void main(String[] args) {
-        try{
+      /*  try{
             
             new Cliff ().climb ();
         } catch (Throwable e) {
@@ -32,6 +42,43 @@ public class Cliff {
                 
                 System.out.println ("Suppressed: " + suppressed);
             }
-        }
+        }*/
+        
+        /*LocalDate hatDay = LocalDate.of (20147, Month.JANUARY,15);
+        DateTimeFormatter f = DateTimeFormatter.ISO_DATE;
+        System.out.println (f.format(hatDay));
+        System.out.println (hatDay.format (f));*/
+       /* LocalDate montyPythonDay = LocalDate.of(2017, Month.MAY, 10);
+        LocalDate aprilFools = LocalDate.of(2018, Month.APRIL, 1);
+        Duration duration = Duration.ofDays(1);
+        LocalDate result = montyPythonDay.minus(duration);
+        System.out.println(result + " " + aprilFools.isBefore(result));*/
+        
+/*        LocalDate date = LocalDate.of(2023, Month.JANUARY, 25);
+        int year = date.getYear();
+        LocalDate polarBearDay = LocalDate.of(2017, 2, 27);
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE;
+        System.out.println(polarBearDay.format(formatter));
+        
+        LocalTime startTime = LocalTime.of (9,0);
+        LocalTime endTime = LocalTime.of (17,30);
+        
+        long hoursBetween = ChronoUnit.HOURS.between (startTime, endTime);
+        System.out.println ("Studen dazischen:" + hoursBetween);*/
+        
+        LocalDate montyPythonDay = LocalDate.of(2017,Month.MAY,10);
+        LocalTime time = LocalTime.of (5,40);
+        LocalDateTime dateTime = LocalDateTime.of(montyPythonDay, time);
+        Duration duration = Duration.ofDays (1);
+        LocalDateTime result = dateTime.minus (duration);
+        System.out.println (result);
+    }
+    
+    
+    public boolean isItMyBirthday(LocalDateTime dateTime){
+        
+        LocalDate now = LocalDate.now ();
+        return now.getMonth () == dateTime.getMonth ()
+                && now.getDayOfMonth () == dateTime.getDayOfMonth ();
     }
 }
